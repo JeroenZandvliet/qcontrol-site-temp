ed62a5f4d823c84b6a1ae0a82610f50134adfb04
Jeroen Zandvliet <jeroen@v-web.nl>
2021-03-23 10:42:44 +0100

    Commit fix

---
+++ libraries/mylib/user.php
@@ -0,0 +1,30 @@
+<?php
+namespace foo;
+
+class MylibUser{
+
+    function getNationalities()
+    {
+        # Initialize Curl call
+        $curl_init = curl_init("https://qcontroldev.mk2softwaredev.nl/api/services/app/Driver/GetNationalities");
+
+        # Set up the Curl request
+        curl_setopt($curl_init, CURLOPT_HEADER, 0);
+
+        # Return response as array instead of Object
+        curl_setopt($curl_init, CURLOPT_HTTP_CONTENT_DECODING, false);
+        curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
+
+        #Store response in $response
+        $response = curl_exec($curl_init);
+
+        #Close Curl response
+        curl_close($curl_init);
+
+        # Decode array response
+        $decoded = json_decode($response, true);
+
+        # Return json_decoded variable
+        return $decoded;
+    }
+}
---
+++ plugins/system/mylib/mylib.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2005 - 2021 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Mylib plugin class.
+ *
+ * @package     Joomla.plugin
+ * @subpackage  System.mylib
+ */
+class plgSystemMylib extends JPlugin
+{
+    /**
+     * Method to register custom library.
+     *
+     * return  void
+     */
+    public function onAfterInitialise()
+    {
+        JLoader::registerPrefix('Mylib', JPATH_LIBRARIES . '/mylib');
+    }
+}
